6a2271e39ad35a956f73f98d6c48aa19b5fab442
syfaarizal <syifairgi@gmail.com>
2026-04-14 10:39:25 +0700

    Refactor CSS blog post to improve structure and clarity. Updates include enhanced section headings, additional examples, and a more cohesive flow of intermediate styling techniques, focusing on flexbox and grid layouts.

---
+++ sicoder-react/src/pages/blog/js-modul/js5Page.tsx
@@ -0,0 +1,3 @@
+export default function Js5Page() {
+  return <div>JavaScript Module - JS 5 Page</div>;
+}
---
+++ sicoder-react/src/pages/blog/js-modul/js5SolutionPage.tsx
@@ -0,0 +1,3 @@
+export default function Js5SolutionPage() {
+  return <div>JavaScript Module - JS 5 Solution Page</div>;
+}
