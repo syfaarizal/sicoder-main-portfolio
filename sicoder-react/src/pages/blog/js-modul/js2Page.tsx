d57911c4f6f4cde97796a0b5939b460f6b0aaad7
syfaarizal <syifairgi@gmail.com>
2026-04-14 10:39:59 +0700

    Refactor CSS blog post to enhance clarity and engagement. Updates include improved section headings, additional examples, and a more cohesive flow of intermediate styling techniques, with a focus on flexbox and grid layouts.

---
+++ sicoder-react/src/pages/blog/js-modul/js2Page.tsx
@@ -0,0 +1,3 @@
+export default function Js2Page() {
+  return <div>JavaScript Module - JS 2 Page</div>;
+}
---
+++ sicoder-react/src/pages/blog/js-modul/js2SolutionPage.tsx
@@ -0,0 +1,3 @@
+export default function Js2SolutionPage() {
+  return <div>JavaScript Module - JS 2 Solution Page</div>;
+}
