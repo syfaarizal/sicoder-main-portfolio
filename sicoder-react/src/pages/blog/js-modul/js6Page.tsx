811b1de04876a16ffe264b2b19b171aa12330f51
syfaarizal <syifairgi@gmail.com>
2026-04-14 10:39:09 +0700

    Refactor CSS blog post structure and content to enhance clarity and engagement. Updates include improved section headings, additional examples, and a more cohesive flow of intermediate styling techniques.

---
+++ sicoder-react/src/pages/blog/js-modul/js6Page.tsx
@@ -0,0 +1,3 @@
+export default function Js6Page() {
+  return <div>JavaScript Module - JS 6 Page</div>;
+}
---
+++ sicoder-react/src/pages/blog/js-modul/js6SolutionPage.tsx
@@ -0,0 +1,3 @@
+export default function Js6SolutionPage() {
+  return <div>JavaScript Module - JS 6 Solution Page</div>;
+}
